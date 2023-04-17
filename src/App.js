e7bae8af5a12a474bda7580f2489e0b4a6475c03
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-04-17 13:23:14 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Auth } from 'aws-amplify';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function signIn() {
+    const user = await Auth.signIn('testuser20', 'testtest');
+    setUser(user);
+  }
+
+  async function signOut() {
+    await Auth.signOut();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +22,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signIn}>Sign In</button>
+        <button onClick={signOut}>Sign Out</button>
+        <p>{user ? JSON.stringify(user) : 'No user'}</p>
       </header>
     </div>
   );
